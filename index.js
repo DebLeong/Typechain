2271c264e9cf94defc32aca14f649b49fc23bf3e
DebLeong <debleon9@gmail.com>
2018-07-14 17:35:38 +0800

    add everything to repo

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-console.log("hello");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const sayHi = (name, age, gender) => {
+    return `Hello ${name}, you are ${age}, you are a ${gender}!`;
+};
+console.log(sayHi("Deborah", 24, "female"));
 //# sourceMappingURL=index.js.map
